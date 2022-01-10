c0f0a01257c1ebefdb449567f4448f6dc9026d6e
CGAnVil <hoangminh.tlu@gmail.com>
2022-01-10 09:50:03 +0700

    upload bai tap

---
+++ src/com/codegym/Students.java
@@ -0,0 +1,21 @@
+package com.codegym;
+
+public class Students {
+    private int rollno;
+    private String name;
+    private static String college = "BBDIT";
+
+
+    Students (int r, String n) {
+        rollno = r;
+        name = n;
+    }
+
+    static void change(){
+        college = "CODEGYM";
+    }
+
+    void display(){
+        System.out.println(rollno + " " + name +  " " + college);
+    }
+}
---
+++ src/com/codegym/TestStaticMethod.java
@@ -0,0 +1,16 @@
+package com.codegym;
+
+public class TestStaticMethod {
+    public static void main(String[] args) {
+        Students.change();
+
+        Students s1 = new Students(111, "Hoang");
+        Students s2 = new Students(222, "Khanh");
+        Students s3 = new Students(333, "Nam");
+
+        s1.display();
+        s2.display();
+        s3.display();
+
+    }
+}
